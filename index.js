d540cda11f6e10e5a101c1fd26c46306d3d843c8
Piscinao <henhvj@gmail.com>
2020-05-27 13:41:57 -0300

    Initial HTML render

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
 
+//motor html para uso ejs
+app.set('view engine', 'ejs');
+
+app.get("/", (req,res) => {
+  res.render("index")
+});
+
+app.listen(8080,()=> {
+  console.log("App is running!");
+});
